707043ffd30e9094b8050e4bbc750088b65c4141
vincip99 <v4palomba99@gmail.com>
2024-11-21 23:42:49 +0100

    update bag script and data

--- save_joint_torque.sh
+++ save_joint_torque.sh
@@ -3,6 +3,9 @@
 # Ensure ROS 2 environment is sourced
 source /opt/ros/humble/setup.bash
 
+# Enter number file
+read -p "Enter a number to add to the file name: " num
+
 # Record specified topics into a bag file named "joint_torque"
 ros2 bag record /effort_controller/commands \
-         -o joint_torques_bag
+         -o "bag_records/joint_torques_bag_${num}"
